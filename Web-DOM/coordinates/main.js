bfd2b0f22b5b1aafd85b19088ded3d32b4a0e61c
itslitulinchpin2 <itslitulinchpin@gmail.com>
2024-03-19 18:04:45 +0100

    DOM handling

---
+++ Web-DOM/coordinates/main.js
@@ -0,0 +1,26 @@
+const vertical = document.querySelector('.vertical');
+const horizontal = document.querySelector('.horizontal');
+const target = document.querySelector('.target');
+const tag = document.querySelector('.tag');
+
+
+addEventListener('load', ()=>{
+    const targetRect = target.getBoundingClientRect();
+    const targetHalfWidth = targetRect.width/2;
+    const targetHalfHeight = targetRect.height/2;
+    
+    document.addEventListener('mousemove',(e)=>{
+        const x = e.clientX;
+        const y = e.clientY
+        console.log(`${x},${y}`);
+    
+    
+        vertical.style.transform = `translateX(${x}px)`
+        horizontal.style.transform = `translateY(${y}px)`
+        target.style.transform = `translate(${x-targetHalfWidth}px,${y-targetHalfHeight}px)`
+        tag.style.transform = `translate(${x+20}px,${y+20}px)`
+        tag.innerHTML=`${x}px,${y}px`;
+    
+    
+    } )
+})
